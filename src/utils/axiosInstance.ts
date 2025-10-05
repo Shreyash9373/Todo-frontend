60400604cc4b1edbde97300263f1c753ac9f18de
shreyas <shreyashraut8@gmail.com>
2025-10-05 20:54:19 +0530

    Added the aws elastic beanstalk url on which backend is deployed

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://todo-backend-3im5.onrender.com/api/v1",
+  // baseURL: "https://todo-backend-3im5.onrender.com/api/v1",
+  baseURL:
+    "http://todo-backend-env.eba-swkiy7ix.ap-south-1.elasticbeanstalk.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-type": "application/json",
